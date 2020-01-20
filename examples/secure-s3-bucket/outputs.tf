3a5d7edccc5807d0ff7fac0402a267627bd767fe
Soren Martius <soeren.martius@gmail.com>
2020-01-20 16:13:09 +0100

    [ci skip] output full bucket details

--- examples/secure-s3-bucket/outputs.tf
+++ examples/secure-s3-bucket/outputs.tf
@@ -1,9 +1,9 @@
-output "example-s3-bucket-arn" {
-  description = "The ARN of our example bucket."
-  value       = module.example-app-bucket.arn
+output "example-s3-bucket" {
+  description = "The outputs ARN of our example bucket."
+  value       = module.example-app-bucket
 }
 
-output "example-s3-log-bucket-arn" {
-  description = "The ARN of our example log bucket."
+output "example-s3-log-bucket" {
+  description = "The outputs of our example log bucket."
   value       = module.example-log-bucket
 }
